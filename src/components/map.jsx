570bb04669db1a70db8685c04dc8b242259fef38
Amir <amirrajabi.neo@gmail.com>
2022-08-17 12:39:23 +1000

    Add GeoJSON

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,15 +1,9 @@
-import { useEffect } from 'react';
-import { MapContainer, GeoJSON, TileLayer, Marker, Popup } from 'react-leaflet'
-
-// import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer } from 'react-leaflet'
+import { GeoJSON } from 'react-leaflet/GeoJSON'
 
 import mapData from './../data/locations.json'
 
 function Map() {
-
-  useEffect(()=>{
-    console.log(mapData)
-  },[])
 
   return (
     <MapContainer center={[40.77,-73.97]} zoom={11} scrollWheelZoom={false} style={{height: '80vh'}}>
@@ -17,13 +11,11 @@ function Map() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {mapData.features.map((area, index) => {
-        const pos = area.geometry.coordinates[0][0]
-        const lat = pos[1]
-        const log = pos[0]
-        return(
-          <Marker key={index} position={[lat, log]}></Marker>
-        )})}
+
+      <GeoJSON data={mapData}>
+        
+      </GeoJSON>
+
     </MapContainer>
   );
 }
