957b4f0c0606b8832c4cc531fcf4799f76025aa6
Amir <amirrajabi.neo@gmail.com>
2022-08-17 10:23:25 +1000

    Add styled components and show map in page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import styled from 'styled-components'
+
+const Box = styled.div({
+  height: 180
+})
 
 function App() {
   return (
-    <div className={{height: '180px'}}>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <Box>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "100%" }} >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -14,7 +19,7 @@ function App() {
           </Popup>
         </Marker>
       </MapContainer>
-    </div>
+    </Box>
   );
 }
 
